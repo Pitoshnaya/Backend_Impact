e77a9e40e5c7cd6b15d477a2691359a6c5a7ae68
Ahmed Gadzhiev <akhmed.34@mail.ru>
2025-05-13 18:43:29 +0300

    Реализована модель Cell и dto для контроллера и сервиса

---
+++ src/main/java/Pitoshnaya/Impact/dto/DrawRequest.java
@@ -0,0 +1,3 @@
+package Pitoshnaya.Impact.dto;
+
+public record DrawRequest (int x, int y, String color) {}
---
+++ src/main/java/Pitoshnaya/Impact/dto/DrawResponse.java
@@ -0,0 +1,3 @@
+package Pitoshnaya.Impact.dto;
+
+public record DrawResponse(String message) {}
