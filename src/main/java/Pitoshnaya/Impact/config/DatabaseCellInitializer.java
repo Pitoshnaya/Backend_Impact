5d435f0b2a18c3c84856554ab770d5c32869f868
Ahmed Gadzhiev <akhmed.34@mail.ru>
2025-05-13 18:34:36 +0300

    Добавлен класс для первичного заполнения базы данных ячейками

---
+++ src/main/java/Pitoshnaya/Impact/config/DatabaseCellInitializer.java
@@ -0,0 +1,30 @@
+package Pitoshnaya.Impact.config;
+
+import Pitoshnaya.Impact.dao.CellsDao;
+import Pitoshnaya.Impact.entity.Cell;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class DatabaseCellInitializer implements CommandLineRunner {
+
+    private final CellsDao cellsDao;
+
+    public DatabaseCellInitializer(CellsDao cellsDao) {
+        this.cellsDao = cellsDao;
+    }
+
+    @Override
+    public void run(String... args) {
+        if (cellsDao.count() > 0) {
+            return;
+        }
+
+        for (int i = 1; i <= 25; i++) {
+            for (int j = 1; j <= 25; j++) {
+                cellsDao.saveCell(new Cell(i, j, "#ffffff"));
+            }
+        }
+    }
+}
