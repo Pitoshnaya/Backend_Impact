4731022fb27132b0973e5083903acf35398dd464
Ahmed Gadzhiev <akhmed.34@mail.ru>
2025-05-21 17:39:04 +0300

    Исправил значения констант длины и высоты

--- src/main/java/Pitoshnaya/Impact/config/GridSize.java
+++ src/main/java/Pitoshnaya/Impact/config/GridSize.java
@@ -1,6 +1,6 @@
 package Pitoshnaya.Impact.config;
 
 public final class GridSize {
-    public static final int WIDTH = 100;
-    public static final int HEIGHT = 100;
+    public static final int WIDTH = 25;
+    public static final int HEIGHT = 25;
 }
