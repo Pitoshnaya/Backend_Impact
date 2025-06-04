4e43f4f3ea030e40de6746ce84278f4b6796a76f
Ahmed Gadzhiev <akhmed.34@mail.ru>
2025-06-04 19:25:22 +0300

    Добавил возможность дебага приложения из контейнера

--- launcher.sh
+++ launcher.sh
@@ -11,4 +11,4 @@ apt-get update && apt-get install -y inotify-tools
 ) &
 
 # start spring boot dev server
-./mvnw spring-boot:run
+./mvnw spring-boot:run -Dspring-boot.run.jvmArguments="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
